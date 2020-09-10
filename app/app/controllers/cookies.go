ab17be810321a9a9c839a1a9319d05e17ee67b55
Manank Patni <manank321@gmail.com>
2020-09-10 18:04:10 +0530

    Add Cookie and Order Endpoints

--- app/app/controllers/cookies.go
+++ app/app/controllers/cookies.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"app/app/models"
+
 	"github.com/revel/revel"
 )
 
+// Cookies Controller with embedded App
 type Cookies struct {
 	App
 }
@@ -14,15 +17,36 @@ var cookies = []Cookie{
 	Cookie{"Blueberry", "This is Blueberry cookie", 12123},
 }
 
-// Cookies Controllers
+// GetCookies is used to get all cookies json
 func (c *Cookies) GetCookies() revel.Result {
+	cookies := []models.Cookie{}
+	result := DB.Find(&cookies)
+	println(result)
 	return c.RenderJSON(cookies)
 }
 
+// CreateCookie is used to create a new cookie item
 func (c *Cookies) CreateCookie() revel.Result {
-	return c.RenderJSON(cookies)
+	var jsonData map[string]interface{}
+	c.Params.BindJSON(&jsonData)
+
+	cookie := models.Cookie{
+		Name:        jsonData["name"].(string),
+		Description: jsonData["description"].(string),
+		Price:       jsonData["price"].(uint),
+		Quantity:    jsonData["quantity"].(uint),
+	}
+
+	result := DB.Create(&cookie)
+	println(result)
+
+	return c.RenderJSON(cookie)
 }
 
+// GetCookie is used to get a single cookie object
 func (c *Cookies) GetCookie(id int) revel.Result {
-	return c.RenderJSON(cookies)
+	cookie := models.Cookie{}
+	result := DB.First(&cookie, id)
+	println(result.RowsAffected)
+	return c.RenderJSON(cookie)
 }
--- app/app/controllers/orders.go
+++ app/app/controllers/orders.go
@@ -1,22 +1,52 @@
 package controllers
 
 import (
+	"app/app/models"
+
 	"github.com/revel/revel"
 )
 
+// Orders Controller with embedded App
 type Orders struct {
 	App
 }
 
-// Order Controllers
+// GetOrders get all current orders
 func (c *Orders) GetOrders() revel.Result {
-	return c.RenderJSON(cookies)
+	orders := []models.Order{}
+	result := DB.Find(&orders)
+	println("the orders are", result)
+	println(result.RowsAffected)
+	return c.RenderJSON(orders)
 }
 
+// GetOrdersUser get all current orders of a user
+func (c *Orders) GetOrdersUser(userid uint) revel.Result {
+	orders := []models.Order{}
+	result := DB.Where("userid = ?", userid).Find(&orders)
+	println("the orders are", result)
+	println(result.RowsAffected)
+	return c.RenderJSON(orders)
+}
+
+// GetOrdersDeliveryAgent get all current orders of delivery agent
+func (c *Orders) GetOrdersDeliveryAgent(deliverypersonid uint) revel.Result {
+	orders := []models.Order{}
+	result := DB.Where("deliverypersonid = ?", deliverypersonid).Find(&orders)
+	println("the orders are", result)
+	println(result.RowsAffected)
+	return c.RenderJSON(orders)
+}
+
+// CreateOrder create a new order
 func (c *Orders) CreateOrder() revel.Result {
 	return c.RenderJSON(cookies)
 }
 
+// GetOrder returns a single order object
 func (c *Orders) GetOrder(id int) revel.Result {
-	return c.RenderJSON(cookies)
+	order := models.Order{}
+	result := DB.First(&order, id)
+	println(result)
+	return c.RenderJSON(order)
 }
